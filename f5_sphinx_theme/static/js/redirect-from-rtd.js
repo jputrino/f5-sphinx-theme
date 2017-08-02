dc7335399e7836fde824fff982d712d166a87468
Jodie Putrino <j.putrino@f5.com>
2017-08-02 14:34:26 -0600

    add redirect script for openstack projects

---
+++ f5_sphinx_theme/static/js/redirect-from-rtd.js
@@ -0,0 +1,8 @@
+/**
+ * Created by putrino on 8/2/17.
+ */
+var domain = "/^.+(openstack).+\.(readthedocs\.io)/";
+
+if (window.location.hostname == "domain") {
+        window.location.assign("http://clouddocs.f5.com/cloud/openstack/");
+    }

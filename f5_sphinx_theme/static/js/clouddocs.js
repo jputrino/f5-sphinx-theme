62a3405640fa8462dad60ce2c1b47b4dd5ed8302
Jodie Putrino <j.putrino@f5.com>
2017-09-27 14:21:15 -0600

    use javascript to get header and footer from clouddocs.f5.com

---
+++ f5_sphinx_theme/static/js/clouddocs.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $("#clouddocs-header").load("http://clouddocs.f5.com/footer.html");
+    $("#clouddocs-footer").load("http://clouddocs.f5.com/footer.html");
+});

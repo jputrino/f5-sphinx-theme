f5f61eb26040f17162a3921156ee8db783f52265
Jodie Putrino <j.putrino@f5.com>
2018-02-14 14:00:16 -0700

    Set up test for theme changes using a dummy docs repo and sending docs to s3 staging bucket with cloudfront

---
+++ test/theme-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+# Clone a test docs repo
+git pull https://$GITHUB_TOKEN:x-oauth-basic@github.com/jputrino/test-deploy.git master
+cd test-deploy
+
+# install dependencies
+pip install --user requirements.txt
+
+# build some test docs
+make -C docs/ html
+
